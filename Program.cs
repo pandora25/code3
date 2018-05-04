63cf7ff92b29dcdcc74eddb33b34cc6b993bfca3
MHY Express <mcyalda@yahoo.com>
2018-05-04 11:52:56 -0400

    1st commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string str = "This C Sharp bootcamp is so AWESOME!";
+            char[] strcount = str.ToCharArray();
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (strcount[i] == 'a' || strcount[i] == 'A')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"the total 'a's are {count}");
+        }
+    }
+}
